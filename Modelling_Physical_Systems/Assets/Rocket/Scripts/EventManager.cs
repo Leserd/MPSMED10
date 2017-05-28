a027a1a6bc5ccc71621e5882ba671ad4d63a2657
Andreas Bork <Ahbork@gmail.com>
2017-05-28 14:11:50 +0200

    Vores MPS projekt

---
+++ Modelling_Physical_Systems/Assets/Rocket/Scripts/EventManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventManager : MonoBehaviour {
+
+    public delegate void RocketStage(int index);
+    public static event RocketStage StartThrust;
+    public static event RocketStage StartRamp;
+    public static event RocketStage StartFirstExplosion;
+
+    public delegate void RocketExplosion(int index, Vector3 point, float force);
+    public static event RocketExplosion Explosion;
+    public static EventManager instance;
+
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+
+    public void RocketThrust(int index)
+    {
+        if(StartThrust != null)
+        {
+            //Debug.Log("Thrust!");
+            StartThrust(index);
+        }
+    }
+
+
+    public void RocketRamp(int index)
+    {
+        if (StartRamp != null)
+        {
+           // Debug.Log("Ramp!");
+            StartRamp(index);
+        }
+    }
+
+
+    public void FirstExplosion(int index)
+    {
+        if (StartFirstExplosion != null)
+        {
+            //Debug.Log("FirstExplosion!");
+            StartFirstExplosion(index);
+        }
+    }
+
+
+    public void StartMovingStars(int index, Vector3 point, float force)
+    {
+        if (Explosion != null)
+        {
+           // Debug.Log("Stars should move!");
+            Explosion(index, point, force);
+        }
+    }
+}
---
+++ Modelling_Physical_Systems/Assets/Rocket/Scripts/Thrust.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Thrust : MonoBehaviour {
+    public float thrustTime = 1.5f;
+    public float rampTime = 2f;
+    public float thrustForce = 50;
+    public float torqueForce = 5;
+    public float timeBeforeExplode = 1.5f;
+    private RocketInfo ri;
+    private Transform thrustPos;
+    private Rigidbody rb;
+    private float startTime;
+
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        EventManager.StartThrust += StartThrust;
+        EventManager.StartRamp += StartRamp;
+        ri = GetComponent<RocketInfo>();
+        thrustPos = ri.thrustPos;
+    }
+
+    private void Start()
+    {
+        //EventManager.instance.RocketRamp(GetComponent<RocketInfo>().fireworksIndex);
+        StartRamp(ri.fireworksIndex);
+    }
+
+    private void OnDestroy()
+    {
+        EventManager.StartThrust -= StartThrust;
+        EventManager.StartRamp -= StartRamp;
+    }
+
+    private void StartThrust(int index)
+    {
+        if(index == ri.fireworksIndex)
+        {
+            StartCoroutine(ThrustCoroutine());
+        }
+    }
+
+
+    private void StartRamp(int index)
+    {
+        if (index == ri.fireworksIndex)
+        {
+            StartCoroutine(RampCoroutine());
+        }
+    }
+
+
+    private IEnumerator RampCoroutine()
+    {
+        yield return new WaitForSeconds(rampTime);
+
+        StartThrust(ri.fireworksIndex);
+        //EventManager.instance.RocketThrust(ri.fireworksIndex);
+    }
+
+
+    private IEnumerator ThrustCoroutine()
+    {
+        startTime = Time.time;
+        float endTime = startTime + thrustTime;
+
+        while (Time.time < endTime)
+        {
+            rb.AddForceAtPosition(thrustPos.up * thrustForce, thrustPos.position, ForceMode.Force);
+            rb.AddRelativeTorque(new Vector3(0, torqueForce, 0));
+            //transform.RotateAround(transform.localPosition, transform.up, torqueForce * Time.fixedDeltaTime);
+
+            yield return new WaitForFixedUpdate();
+        }
+        //Debug.Log("Thrust stopped");
+
+        yield return new WaitForSeconds(timeBeforeExplode);
+        ri.thrustPos.GetComponent<TrailRenderer>().enabled = false;
+        EventManager.instance.FirstExplosion(ri.fireworksIndex);        
+    }
+
+}
